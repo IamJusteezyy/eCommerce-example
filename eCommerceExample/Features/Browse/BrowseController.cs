f6e5cd13f5b5f94960b942f16d67131c247f2651
Justin Flory <jus.flory500@gmail.com>
2025-05-13 19:23:34 -0500

    Initial Development. Wireframed home page with carousel and picture links.

---
+++ eCommerceExample/Features/Browse/BrowseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceExample.Features.Browse
+{
+    [Route("Browse")]
+    public class BrowseController : Controller
+    {
+        public BrowseController() 
+        {
+        
+        }
+
+        public IActionResult Browse()
+        {
+            return View();
+        }
+    }
+}
